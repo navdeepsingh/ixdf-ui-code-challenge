9a96c4c4971b65849d76f34c2415b83422b5bbba
Navdeep Singh <navdeep.er@gmail.com>
2020-12-09 17:36:25 +0530

    Day5: Progress Save

--- src/index.js
+++ src/index.js
@@ -6,36 +6,47 @@ console.log('Project Loaded!');
 const buttonShowNavigator = document.querySelector('[data-show-lesson-navigator]');
 const buttonHideNavigator = document.querySelector('[data-hide-lesson-navigator]');
 const lessonNavigator = document.querySelector('[data-lesson-navigator]');
+const lessonNavigatorButtons = lessonNavigator.querySelectorAll('button');
 const activeSubLesson = document.querySelector('aside ul li ul li.active');
 const mainContent = document.querySelector('[data-main-content');
 const hamBurgerMenu = document.querySelector('[data-hamburger-menu]');
 const mobileMenu = document.querySelector('header nav');
 
-// Show/Hide Lesson Navigator
+// Show Lesson Navigator
 buttonShowNavigator.addEventListener('click', () => {
     lessonNavigator.classList.add('show');
     buttonShowNavigator.classList.remove('show');
     mainContent.classList.remove('hidden-navigator');
+    // Add LessonNavigator to A11ty tree
+    toggleA11ty(false);
 });
 
+// Hide Lesson Navigator
 buttonHideNavigator.addEventListener('click', () => {
     lessonNavigator.classList.remove('show');
     lessonNavigator.classList.add('hide');
     buttonShowNavigator.classList.add('show');
     mainContent.classList.add('hidden-navigator');
+    // Remove LessonNavigator Buttons from A11ty tree
+    toggleA11ty();
 });
 
 // Toggle Lessons
-const accordionHeaders = document.querySelectorAll('[data-lesson-navigator-header]');
-accordionHeaders.forEach((accordionHeader) => {
-    let target = accordionHeader.parentElement.nextElementSibling;
-    let accordionListItem = accordionHeader.parentElement.parentElement;
-    accordionHeader.onclick = () => {
-        let expanded = accordionHeader.getAttribute('aria-expanded') === 'true' || false;
-        accordionHeader.setAttribute('aria-expanded', !expanded);
-        target.hidden = expanded;
-        if (!expanded) accordionListItem.classList.add('active');
-        else accordionListItem.classList.remove('active');
+var lastClickedLesson = null;
+const accordionList = document.querySelectorAll('[data-lesson-navigator-list]');
+accordionList.forEach((accordionListItem) => {
+    accordionListItem.onclick = () => {
+        // Hide last opened Accordion Lesson Item
+        // Also if same item **not** clicked again
+        if (lastClickedLesson && lastClickedLesson !== accordionListItem) {
+            closeLastItem(lastClickedLesson);
+        }
+
+        // Toggle Show/Hide
+        toggleAccordionItem(accordionListItem);
+
+        // Track last clicked lesson
+        lastClickedLesson = accordionListItem;
     };
 });
 
@@ -51,6 +62,21 @@ quizElements.map((quizElement) => {
     validationRadios(quizElement.radio, quizElement.submit);
 });
 
+// Mobile Menu
+hamBurgerMenu.onclick = (e) => {
+    e.preventDefault();
+    let expanded = hamBurgerMenu.getAttribute('aria-expanded') === 'true' || false;
+    hamBurgerMenu.setAttribute('aria-expanded', !expanded);
+    if (!expanded) mobileMenu.classList.add('show');
+    else mobileMenu.classList.remove('show');
+};
+
+/**
+ *
+ * @param {*} radios : String
+ * @param {*} submitButton : String
+ * return void;
+ */
 function validationRadios(radios, submitButton) {
     const button = document.getElementById(submitButton);
     var radios = document.getElementsByName(radios);
@@ -65,17 +91,49 @@ function validationRadios(radios, submitButton) {
         button.classList.add('submitted');
         button.innerText = 'Submitted';
         if (totalQuestionSubmit === quizElements.length) {
+            // Update State of Active Lesson
             activeSubLesson.firstElementChild.firstElementChild.focus();
             activeSubLesson.classList.add('completed');
         }
     };
 }
 
-// Mobile Menu
-hamBurgerMenu.onclick = (e) => {
-    e.preventDefault();
-    let expanded = hamBurgerMenu.getAttribute('aria-expanded') === 'true' || false;
-    hamBurgerMenu.setAttribute('aria-expanded', !expanded);
-    if (!expanded) mobileMenu.classList.add('show');
-    else mobileMenu.classList.remove('show');
-};
+/**
+ *
+ * @param {*} $set : Boolean
+ * return void;
+ */
+function toggleA11ty($set = true) {
+    lessonNavigatorButtons.forEach((button) => {
+        if ($set) {
+            button.setAttribute('tabindex', -1);
+        } else {
+            button.removeAttribute('tabindex');
+        }
+    });
+}
+
+/**
+ *
+ * @param {*} navigatorItem Node
+ * return void
+ */
+function toggleAccordionItem(navigatorItem) {
+    let accordionButton = navigatorItem.firstElementChild.firstElementChild;
+    let target = navigatorItem.firstElementChild.nextElementSibling;
+
+    let expanded = accordionButton.getAttribute('aria-expanded') === 'true' || false;
+    accordionButton.setAttribute('aria-expanded', !expanded);
+    target.hidden = expanded;
+    if (!expanded) navigatorItem.classList.add('active');
+    else navigatorItem.classList.remove('active');
+}
+
+function closeLastItem(navigatorItem) {
+    let accordionButton = navigatorItem.firstElementChild.firstElementChild;
+    let target = navigatorItem.firstElementChild.nextElementSibling;
+
+    accordionButton.setAttribute('aria-expanded', false);
+    target.hidden = true;
+    navigatorItem.classList.remove('active');
+}
